eea940f68950856b9b46a8d32f89525c9e2ae323
Your Name <your_email@example.com>
2025-08-21 20:23:26 +0300

    add Colors app

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primre = Color(0xffFA9A0C);
+  static const Color grey = Color(0xff9D9FA0);
+  static const Color black = Color(0xff111111);
+  static const Color white = Color(0xffFFFFFF);
+}
