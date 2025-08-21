b2628b444f96c3a312c718f73576ff1286b29f40
Your Name <your_email@example.com>
2025-08-21 20:45:21 +0300

    add image app

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,8 @@
+class AssetsApp {
+  static const String logo = 'assets/images/logo.png';
+  static const String bg1 = 'assets/images/bg 1.png';
+  static const String bg2 = 'assets/images/background_image 1.png';
+  static const String lock = 'assets/images/lock 1.png';
+  static const String avatar = 'assets/images/avatar.png';
+  static const String icon = 'assets/images/icon (2).png';
+}
