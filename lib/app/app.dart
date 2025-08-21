85eb402fede6e5cb584ad67ba0aae2d76aa9400a
Your Name <your_email@example.com>
2025-08-21 19:28:27 +0300

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChefApp extends StatelessWidget {
+  const ChefApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
